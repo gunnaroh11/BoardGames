5a39510bba723b94da47f362d63e087e2cea8838
Gunnar Örn Haraldsson <gunnaroh11@gmail.com>
2013-05-15 17:52:04 +0000

    GameEngine breytt

--- BoardGames/GameEngine.cpp
+++ BoardGames/GameEngine.cpp
@@ -8,9 +8,10 @@ void list(vector<Game> G)
 {
 	for(int i = 0;i< (int)G.size();i++)
 	{
-	//cout lists
+		cout << G[i].Name;
 	}
 };
+
 Game game(int n,vector<Game> G)
 {
 	return G[n];
@@ -26,20 +27,28 @@ string word;
 
 cout << "type list to get a list of all supported games" << endl;
 cout << "type game to pick game to play" << endl;
-while(	cin>>word)
-	if(word == "list")
+	while(true)
 	{
-		list(Games);
-	}
-	if(word == "game")
-	{
-		cout << "pick the number of the game you want to play"<<endl;
-		int nr;
-		cin>>nr;
-		Game ChosenGame = game(nr,Games);
-		ChosenGame.start();
-	}
-cout << "type list to get a list of all supported games" << endl;
-cout << "type game to pick game to play" << endl;
 
+		cin >> word;	
+
+		if(word == "list")
+		{				
+			if(!Games.empty())
+				list(Games);
+			else
+				cout << "No games avalible" << endl;
+		}
+
+		if(word == "game")
+		{
+			cout << "pick the number of the game you want to play"<<endl;
+			int nr;
+			cin>>nr;
+			Game ChosenGame = game(nr,Games);
+			ChosenGame.start();
+		}
+		cout << "type list to get a list of all supported games" << endl;
+		cout << "type game to pick game to play" << endl;
+}
 }
