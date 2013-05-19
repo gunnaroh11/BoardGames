828c9682278a125345ce863236a120c13ce9f2c8
Gunnar Örn Haraldsson <gunnaroh11@gmail.com>
2013-05-19 13:38:55 +0000

    lagf og vinna í kommentum

--- BoardGames/Game.h
+++ BoardGames/Game.h
@@ -19,19 +19,19 @@ using namespace std;
 class Game
 {
 public:
-	//!< Default Game constructor							
+	//! Default Game constructor 	
 	Game();					
 
 	//!< Virtual void, returns legal moves for piece in current game with moves ref
 	/*!
 		\param &legalMoves a reference to vector of Point. Returns legal moves in vector
-		\param &piece a reference to piece being moved
-		\return The test results		
+		\param &piece a reference to piece being moved	
 	*/
 	virtual void legal(vector<Point> &legalMoves, Piece &piece){}; 
 	
 	//!< Virtual void to start game
 	virtual void start();	
+
 	//!< Virtual void to make move, move 1 piece from cord to other cord
 	/*!
 		\param FromX x value for piece to be moved
@@ -40,32 +40,69 @@ public:
 		\param ToY y value for destination
 	*/
 	virtual void make(int FromX,int FromY,int ToX,int ToY); 
-	virtual void go();				//!< Virtual void function to let cpu make move according to difficult level
-	void retract();			//!< Retract last move, pops GameBoard state and sets game to last state
-	virtual void display(); //!< Display board
-	
-	virtual void evaluate();		//!< Virtual void, evaluate if player can make move in current position and set score
-	void debug();					//!< Switch between debug mode and non-debug mode
-	void quit();					//!< Exit program
-	string GetName();				//!< Returns name 
-	void setDifficulty(int diff);	//!< Sets difficult state 0 = random, 1 = easy, 2 = medium, 3 = hard
-	int getDifficulty();			//!< Returns current difficulty level
-	void GetTestGame();				//!< Runs Test game
 
-	bool m_finished;		/**< Indicates if still possible to make moves or if game has finished */
-	void checkFinished();			//!< Prints out result if game has finished
-	bool Debug;				/**< Toggle switch for debugging, if true program prints debug info  */
-	Board GameBoard;		/**< Game instance of class GameBoard */
-	bool canMake(Piece *piece, Point p);	//!< Cheks if piece can make move to cord p
+	//!< Virtual void function to let cpu make move according to difficult level
+	virtual void go();				
+	
+	//!< Retract last move, pops GameBoard state and sets game to last state
+	void retract();			
+	
+	//!< Display board
+	virtual void display(); 
+	
+	//!< Virtual void, evaluate if player can make move in current position and set score
+	virtual void evaluate();		
+	
+	//!< Switch between debug mode and non-debug mode
+	void debug();					
+	
+	//!< Exit program
+	void quit();		
+
+	//!< Returns name 
+	string GetName();				
+
+	//!< Sets difficult state 0 = random, 1 = easy, 2 = medium, 3 = hard
+	void setDifficulty(int diff);	
+
+	//!< Returns current difficulty level
+	int getDifficulty();			
+
+	//!< Runs Test game
+	void GetTestGame();				
+
+	/**< Indicates if still possible to make moves or if game has finished */
+	bool m_finished;		
+
+	//!< Prints out result if game has finished
+	void checkFinished();	
+
+	/**< Toggle switch for debugging, if true program prints debug info  */
+	bool Debug;				
+
+	/**< Game instance of class GameBoard */
+	Board GameBoard;		
+
+	//!< Cheks if piece can make move to cord p
+	bool canMake(Piece *piece, Point p);	
+
 protected:
-	string GameName;		/**< Name of the game */
-	int player1_pieces;		/**< Amount of pieces for player 1 */
-	int player2_pieces;		/**< Amount of pieces for player 2 */
-	int difficulty;			/**< Keeps difficulty level */
-	vector<Board> history;	/**< Vector of each game state, changes each turn */
-	int MaxPlays;			/**< Keeps max moves in game */
-	int CurrentTurn;		/**< Keeps current move, counts each move */
-	int CurrentPlayer;		/**< Indicates who´s player turn it is */
+	/**< Name of the game */
+	string GameName;		
+	/**< Amount of pieces for player 1 */
+	int player1_pieces;		
+	/**< Amount of pieces for player 2 */
+	int player2_pieces;		
+	/**< Keeps difficulty level */
+	int difficulty;			
+	/**< Vector of each game state, changes each turn */
+	vector<Board> history;	
+	/**< Keeps max moves in game */
+	int MaxPlays;			
+	/**< Keeps current move, counts each move */
+	int CurrentTurn;		
+	/**< Indicates who´s player turn it is */
+	int CurrentPlayer;		
 
 };
 #endif
---
+++ BoardGames/html/search/functions_62.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['breaktrough',['Breaktrough',['../class_breaktrough.html#a988083f0722bff1058a980748c65705f',1,'Breaktrough']]]
+];
